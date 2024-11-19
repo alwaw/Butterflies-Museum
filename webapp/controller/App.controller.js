6480391f18dff8f486ae701ba76a867f4e597e42
alwaw <124403241+alwaw@users.noreply.github.com>
2024-11-19 18:12:08 +0100

    Deleting table rows

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,20 @@
 sap.ui.define(
   [
     "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
     "sap/ui/model/FilterType",
   ],
-  function (Controller, JSONModel, Filter, FilterOperator, FilterType) {
+  function (
+    Controller,
+    MessageToast,
+    JSONModel,
+    Filter,
+    FilterOperator,
+    FilterType
+  ) {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.App", {
@@ -17,11 +25,11 @@ sap.ui.define(
 
       // Event handler for sap.m.SearchField and filter
       onSearch: function () {
-        const oView = this.getView(),
-          sValue = oView.byId("searchField").getValue(),
-          fValue = parseFloat(sValue); // convert value to number
+        const oView = this.getView();
+        const sValue = oView.byId("searchField").getValue();
+        const fValue = parseFloat(sValue); // convert value to number
 
-        //only string filters, mapping throught the all columns with string value:
+        //only string filters, mapping through the all columns with string value:
         const aColumns = [
           "GUID",
           "Name",
@@ -51,6 +59,30 @@ sap.ui.define(
           .byId("idTheMuseumOfButterflies")
           .getBinding("rows")
           .filter(oCombinedFilter, FilterType.Application);
+      },
+
+      // Event handler for deleting selected row
+      onDelete: function () {
+        const oTable = this.byId("idTheMuseumOfButterflies");
+        const aSelectedIndices = oTable.getSelectedIndices(); //get selected rows
+
+        if (aSelectedIndices.length > 0) {
+          const oModel = this.getView().getModel("butterflies");
+          const aDataArray = oModel.getProperty("/butterflies");
+
+          // reverse the selected indices array to delete rows safely:
+          aSelectedIndices.reverse().forEach((iIndex) => {
+            aDataArray.splice(iIndex, 1);
+          });
+
+          //update the model with the modified data:
+          oModel.setProperty("/butterflies", aDataArray);
+
+          //succes message:
+          sap.m.MessageToast.show("Selected rows deleted succesfully.");
+        } else {
+          sap.m.MessageToast.show("Please select at least one row to delete");
+        }
       },
     });
   }
