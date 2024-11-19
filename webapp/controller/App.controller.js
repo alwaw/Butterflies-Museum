c94d3dd5ac159a52e3925411fd02ec83a721cb8c
alwaw <124403241+alwaw@users.noreply.github.com>
2024-11-19 11:38:10 +0100

    Filtering in a Single Column
    Filtering in a Single Column and Code Optimization - Adding .map to Generate Filters

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,7 +6,7 @@ sap.ui.define(
     "sap/ui/model/FilterOperator",
     "sap/ui/model/FilterType",
   ],
-  function (Controller, JSONModel, Sorter, Filter, FilterOperator, FilterType) {
+  function (Controller, JSONModel, Filter, FilterOperator, FilterType) {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.App", {
@@ -15,57 +15,37 @@ sap.ui.define(
         this.getView().setModel(oModel, "butterflies");
       },
 
-      // Event handler for sap.m.SearchField
+      // Event handler for sap.m.SearchField and filter
       onSearch: function () {
         const oView = this.getView(),
           sValue = oView.byId("searchField").getValue(),
-          fValue = parseFloat(sValue), // convert value to number
-          //może dodać mapowanie zamiast pojedynczych filtrów?
-          //only string filters:
-          oFilter1 = new Filter("GUID", FilterOperator.Contains, sValue),
-          oFilter2 = new Filter("Name", FilterOperator.Contains, sValue),
-          oFilter3 = new Filter("Family", FilterOperator.Contains, sValue),
-          oFilter4 = new Filter("Location", FilterOperator.Contains, sValue),
-          oFilter5 = new Filter("Date", FilterOperator.Contains, sValue),
-          oFilter6 = new Filter("Wingspan", FilterOperator.Contains, sValue),
-          oFilter7 = new Filter("Weight", FilterOperator.Contains, sValue),
-          oFilter8 = new Filter("Price", FilterOperator.Contains, sValue),
-          oFilter9 = new Filter("Habitat", FilterOperator.Contains, sValue),
-          oFilter10 = new Filter("Lifespan", FilterOperator.Contains, sValue),
-          oFilter11 = new Filter(
-            "Migration Pattern",
-            FilterOperator.Contains,
-            sValue
-          ),
-          oFilter12 = new Filter("Location", FilterOperator.Contains, sValue),
-          oFilter13 = new Filter(
-            "Threat Level",
-            FilterOperator.Contains,
-            sValue
-          ),
-          // number filters:
-          oFilter14 = new Filter("Abundance", FilterOperator.EQ, fValue),
-          oFilter15 = new Filter("Color Rating", FilterOperator.EQ, fValue);
-        const oCombinedFilter = new Filter({
-          filters: [
-            oFilter1,
-            oFilter2,
-            oFilter3,
-            oFilter4,
-            oFilter5,
-            oFilter6,
-            oFilter7,
-            oFilter8,
-            oFilter9,
-            oFilter10,
-            oFilter11,
-            oFilter12,
-            oFilter13,
-            oFilter14,
-            oFilter15,
-          ],
-          and: false,
+          fValue = parseFloat(sValue); // convert value to number
+
+        //only string filters, mapping throught the all columns with string value:
+        const aColumns = [
+          "GUID",
+          "Name",
+          "Family",
+          "Location",
+          "Date",
+          "Wingspan",
+          "Weight",
+          "Price",
+          "Habitat",
+          "Lifespan",
+          "Migration Pattern",
+          "Threat Level",
+        ];
+        const aFilters = aColumns.map((sColumn) => {
+          return new Filter(sColumn, FilterOperator.Contains, sValue);
         });
+
+        const aNumberColumns = ["Abundance", "Color Rating"];
+        aNumberColumns.forEach((sColumn) => {
+          aFilters.push(new Filter(sColumn, FilterOperator.EQ, fValue));
+        });
+
+        const oCombinedFilter = new Filter({ filters: aFilters, and: false });
 
         oView
           .byId("idTheMuseumOfButterflies")
